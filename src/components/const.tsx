84d366e1cefb4b26efac79bd27ab17303ab663a0
andre-de-kaymagne <akaymanov@yandex.ru>
2023-10-24 23:07:32 +0300

    Добавил консты

--- src/components/const.tsx
+++ src/components/const.tsx
@@ -1,5 +1,18 @@
-const Settings = {
-  PlacesCount: 12
+export const Settings = {
+  PlacesCount: 6
 } as const;
 
-export{Settings};
+export enum AppRoute {
+  Root = '/',
+  Login = '/login',
+  Favorites = '/favorites',
+  Offer = '/offer',
+}
+
+export enum AuthorizationStatus {
+  Auth = 'AUTH',
+  NotAuth = 'NOT_AUTH',
+  Unknown = 'UNKNOWN',
+}
+
+
